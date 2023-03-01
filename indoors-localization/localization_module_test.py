0f082ecd45ebe8f5f9c7a1016a3791d0f34af98f
rbezares5 <rbezares5@gmail.com>
2023-03-01 13:06:56 +0100

    Implemented batch localization in a class

---
+++ indoors-localization/localization_module_test.py
@@ -0,0 +1,29 @@
+"""
+This script showcases how to solve the localization problem making use of
+the functions implemented in python classes
+"""
+
+from loc_module import EnvironmentModel
+
+def main():
+    my_map=EnvironmentModel('Friburgo/Friburgo_Train/*.jpeg')
+    my_map.import_test_images('Friburgo/Friburgo_Test_ext/*.jpeg')
+
+    #my_map.get_map_coords()
+    #my_map.export_map_coords()
+    my_map.import_map_coords('map_coordinates.csv')
+
+    #my_map.create_hog_map()
+    #my_map.export_map_desciptors('HOG')
+    my_map.import_map_descriptors('HOG_model.csv')
+
+    #my_map.online_hog_test()
+    #my_map.export_test_results('HOG')
+    my_map.import_test_results('batch_location_HOG.csv')
+
+    my_map.show_neighbours_histogram()
+
+
+
+if __name__ == "__main__":
+    main()
